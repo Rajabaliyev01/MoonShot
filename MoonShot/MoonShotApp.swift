e77b12fac3cc07edc9e7ae47142fd0528eccdffc
Rajabaliyev01 <javohirrajabaliyev@gmail.com>
2026-04-19 20:09:24 +0500

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Rajabaliyev01 on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
